c6682335494dc0d359f6767dfe23ac9bd140eb90
Florencia Ramirez <floramirez1997@proton.me>
2024-08-12 00:33:05 -0300

    solucion del mensaje del div al clickear el boton

--- eventos.js
+++ eventos.js
@@ -1,10 +1,20 @@
+//para el click del boton
 function saludar() {
     alert("Hola!");
 }
 
-const element2 = document.getElementsByTagName("div");
-element.addEventListener("click", saludar2);
-
-function saludar2(){
-    alert("Hola soy el div!");
+// mensaje del div
+function saludar2(event) {
+    // Check if the click was inside the button, if not, show the alert
+    if (!event.target.closest("#pulsar")) {
+        alert("Hola soy el div!");
+    }
 }
+
+// funcionalidad del div
+const container = document.getElementById("container");
+container.addEventListener("click", saludar2);
+
+// funcionalidad del boton
+const button = document.getElementById("pulsar");
+button.addEventListener("click", saludar);
